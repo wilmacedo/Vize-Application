27df4612b64e5fe27728b6965d148d62dd4e626e
wilmacedo <wil.macedo.sa@gmail.com>
2020-02-06 17:16:17 -0300

    Resolvendo bug de importação de fontes custom

--- App.js
+++ App.js
@@ -1,29 +1,40 @@
-import React from 'react';
-import { Plataform, StyleSheet, StatusBar, View } from 'react-native';
+import React, { useState } from 'react';
+
+import { Platform, StyleSheet, StatusBar, View } from 'react-native';
+
 import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 import color from './src/constants/color';
 import Home from './src/pages/Home';
 
-export default class App extends React.Component {
-  componentDidMount() {
-    Font.loadAsync({
-      'roboto': require('./assets/fonts/Roboto-Regular.ttf'),
-      'roboto-light': require('./assets/fonts/Roboto-Light.ttf'),
-      'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
-    })
-  }
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'roboto': require('./assets/fonts/Roboto-Regular.ttf'),
+    'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    'roboto-light': require('./assets/fonts/Roboto-Light.ttf')
+  });
+};
 
-  render() {
+export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
     return (
-      <>
-        <StatusBar barStyle="light-content" color={color.warning}/>
-        <View style={style.bg}>
-          <Home />
-        </View>
-      </>
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+      />
     );
   }
+  return (
+    <>
+      <StatusBar barStyle="light-content" color={color.warning} />
+      <View style={style.bg}>
+        <Home />
+      </View>
+    </>
+  );
 }
 
 const style = StyleSheet.create({
